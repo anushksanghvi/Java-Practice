9d3d857cf945a728b7b948c54c04eae50953ec31
Anushk <anushksanghvi@gmail.com>
2023-08-01 00:17:43 +0530

    Reverse the given Number added

--- Tutorials/ReverseNum.java
+++ Tutorials/ReverseNum.java
@@ -1,15 +1,28 @@
 import java.util.Scanner;
-
 public class ReverseNum {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
+        // System.out.print("Enter n: ");
+        // int n = sc.nextInt();
+
+        // while(n>0){
+        //     int lastdigit = n % 10;
+        //     System.out.print(lastdigit);
+        //     n = n/10;
+        // }
+        // System.out.println();
+
+//--------------------------------------------------------------------------------        
+
         System.out.print("Enter n: ");
         int n = sc.nextInt();
+        int rev = 0;
+
         while(n>0){
             int lastdigit = n % 10;
-            System.out.print(lastdigit);
+            rev = (rev * 10) + lastdigit;
             n = n/10;
         }
-        System.out.println();
+        System.out.println(rev);
     }
 }
