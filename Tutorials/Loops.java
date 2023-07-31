2f272e354b8897ddaf59f04f4e96623a9b1e8866
Anushk <anushksanghvi@gmail.com>
2023-08-01 01:38:15 +0530

    Loops added

--- Tutorials/Loops.java
+++ Tutorials/Loops.java
@@ -12,13 +12,24 @@ public class Loops {
 
         // System.out.println("Printed Hello World 10x");
 
-
-        // FOR LOOP
-        for(int i=1; i<=10; i++){
-            System.out.println("Hello World! " + i);
-        }
+//-------------------------------------------------------------------------------
+        // // FOR LOOP
+        // for(int i=1; i<=10; i++){
+        //     System.out.println("Hello World! " + i);
+        // }
         
-        System.out.println("printed Hello World 10x");
+        // System.out.println("printed Hello World 10x");
+
+//-------------------------------------------------------------------------------
+        // // DO WHILE LOOP
+        
+        int i = 0;
+        do{
+            System.out.println("Hello World!" + i);
+            i++;
+        }
+        while(i < 10);
+
 
     }       
 }
