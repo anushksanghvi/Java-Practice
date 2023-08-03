c9a650618d78dcd72474cf4c767bc069a61e15d0
Anushk <anushksanghvi@gmail.com>
2023-08-03 22:11:50 +0530

    Continue Statement Example added

---
+++ Tutorials/ContinueExample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ContinueExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        do{
+            System.out.print("Enter your number: ");
+            int num = sc.nextInt();
+            if(num % 10 == 0){
+                continue;
+            }
+            System.out.println("Your number is " + num);
+        }
+        while(true);
+    
+    }
+}
