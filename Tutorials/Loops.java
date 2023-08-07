feb3f4555eb93e688de1c2e51285dc7fe33aba04
Anushk <anushksanghvi@gmail.com>
2023-08-07 23:54:36 +0530

    Loops added

--- Tutorials/Loops.java
+++ Tutorials/Loops.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Loops {
     public static void main(String[] args) {
         
         // // WHILE LOOP
+
         // int i = 0;
         // while (i < 10){
         //     System.out.println(" Hello World! " + i);
@@ -12,15 +13,20 @@ public class Loops {
 
         // System.out.println("Printed Hello World 10x");
 
+
 //-------------------------------------------------------------------------------
+
         // // FOR LOOP
+
         // for(int i=1; i<=10; i++){
         //     System.out.println("Hello World! " + i);
         // }
         
         // System.out.println("printed Hello World 10x");
 
+
 //-------------------------------------------------------------------------------
+
         // // DO WHILE LOOP
         
         int i = 0;
