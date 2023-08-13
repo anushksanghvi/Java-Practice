38efec09523fa4f325241c70ce6e87c78e0d6117
Anushk <anushksanghvi@gmail.com>
2023-08-13 23:56:47 +0530

    Inverted Star Pattern added

--- Pattern/InvertedStarPattern.java
+++ Pattern/InvertedStarPattern.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class InvertedStarPattern {
     public static void main(String[] args) {
-       for(int line=1; line<=5; line++){
-            for (int star=1; star<=line; star++){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Enter no. of line n = ");
+        int n = sc.nextInt();
+
+        for(int line=1; line<=n; line++){
+            for (int star=1; star<=n-line+1; star++){
                 System.out.print("*");
             }
             
