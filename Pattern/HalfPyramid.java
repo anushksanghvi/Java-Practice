432b38f23b25ca0e48803a0f22f44d0e28912764
Anushk <anushksanghvi@gmail.com>
2023-08-15 23:42:58 +0530

    Half Pyramid of number added

--- Pattern/HalfPyramid.java
+++ Pattern/HalfPyramid.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 public class HalfPyramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        System.out.print("Enter no. of line n = ");
+        System.out.print("Enter no. of lines: ");
         int n = sc.nextInt();
 
-        for(int line=n; line>=1; line--){
-            for(int star=1; star<=line; star++){
-                System.out.print("*");
+        for (int line=1; line<=n; line++){
+            for (int num=1; num<=line; num++){
+                System.out.print(num);
             }
             System.out.println();
-        }  
+        }
     }
 }        
