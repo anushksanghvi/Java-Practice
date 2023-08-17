c16b091b667bca228d98a6b35e6d0d62d727e31f
Anushk <anushksanghvi@gmail.com>
2023-08-17 09:58:45 +0530

    Factorial added

--- Function & Methods/Factorial.java
+++ Function & Methods/Factorial.java
@@ -1,18 +1,23 @@
-import java.util.*;
-public class Factorial {
+import java.util.Scanner;
 
-    public static int factorial(int n){
+public class Factorial {
+    public static int printFactorial(int n){
         int f = 1;
-        for(int i=1; i<=n; i++){
-            f = f*i;
+        if (n<0){
+            System.out.println("Invalid number");
+        }
+        else{        
+            for(int i=1; i<=n; i++){
+                f = f * i;
+            }
         }
         return f;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
         System.out.print("Enter value of n = ");
         int n = sc.nextInt();
-        System.out.print("Factorial = " + factorial(n)); 
-    
+        System.out.println("Factorial = " + printFactorial(n));
     }
 }
