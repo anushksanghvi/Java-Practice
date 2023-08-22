959a57fcfdfdfe15b1cffda2d2edaab42c654863
Anushk <anushksanghvi@gmail.com>
2023-08-23 02:10:22 +0530

    Getter- Setter added

--- OOPs/GetterSetter.java
+++ OOPs/GetterSetter.java
@@ -1,33 +1,35 @@
 public class GetterSetter {
     public static void main(String[] args) {
-        Pen p1 = new Pen();
-        p1.color = "red";
-        p1.quantity = 5;
-        System.out.println(p1.color);
-        System.out.println(p1.quantity);        
+        Pen p1 = new Pen(); 
+        
+        p1.setColor("Blue");
+        System.out.println(p1.getColor());  //we need to use getColor() bcoz we cannot print directly (by p1.color) as our string color is private
+
+        p1.setQuantity(14);
+        System.out.println(p1.quantity);
+
+        p1.setColor ("Red");
+        System.out.println(p1.getColor());
+
     }
-}
+}        
 
 class Pen{
-    String color;
-    String type;
+    private String color;
     int quantity;
 
     String getColor(){
         return this.color;
     }
-    String getType(){
-        return this.type;
-    }
     int getQuantity(){
         return this.quantity;
     }
 
-    String setColor(){
-        return this.color;
+    void setColor(String newColor){
+        this.color = newColor;
     } 
-    int setQuantity(){
-        return this.quantity;
+    void setQuantity(int newQuantity){
+        this.quantity = newQuantity;
     }
 
 }
