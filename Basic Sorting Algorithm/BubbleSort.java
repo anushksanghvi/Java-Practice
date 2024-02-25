b4d8bf4f04b0a41133d7aca38c9687bf2478018b
anushksanghvi <anushksanghvi@gmail.com>
2024-02-26 02:51:28 +0530

    Bubble Sort Algorithm

--- Basic Sorting Algorithm/BubbleSort.java
+++ Basic Sorting Algorithm/BubbleSort.java
@@ -4,6 +4,7 @@ public class BubbleSort {
         // int totalSwaps = 0;
 
         for (int turn=0; turn<arr.length-1; turn++) {
+            boolean swapped =false;
             // int swapsInTurn = 0;
 
             for (int j=0; j<arr.length-1-turn; j++) {
@@ -13,18 +14,22 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swapped = true;
                     // swapsInTurn++;
                     // totalSwaps++;
                 }
             }
 
+            if(swapped==false){
+                break;
+            }
             // System.out.println("Swaps in turn " + (turn) + ": " + swapsInTurn);
         }
 
         // System.out.println("Total number of swaps: " + totalSwaps);
     }
 
-    public static void printArray(int arr[]) {
+    public static void printArray(int arr[]) {      //For printing the Array
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
@@ -32,7 +37,8 @@ public class BubbleSort {
     }
 
     public static void main(String[] args) {
-        int arr[] = {5, 4, 1, 3, 2};
+        int arr[] = {12, 45, 23, 51, 19, 8};
+        // int arr[] = {1, 2, 3, 4, 5};
         bubbleSort(arr);
         printArray(arr);
     }
