235d636ff80bc0c47c86fd874967a35941b290f8
anushksanghvi <anushksanghvi@gmail.com>
2024-02-26 02:52:19 +0530

    Selection Sort Algorithm

---
+++ Basic Sorting Algorithm/SelectionSort.java
@@ -0,0 +1,32 @@
+public class SelectionSort {
+
+    public static void selectionSort(int arr[]){
+        int maxNum = Integer.MAX_VALUE;
+
+        for(int i=0; i<arr.length-1; i++){
+            int minPos = i;
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            //swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] =temp;
+
+        }
+    }
+
+    public static void printArray(int arr[]){     //For printing the Array
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] ={5,4,1,3,2};
+        selectionSort(arr);
+        printArray(arr);
+
+    }
+}
