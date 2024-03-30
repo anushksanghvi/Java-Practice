7679e7f41f98e1fc2a4674b95730754788722f4d
Anushk <anushksanghvi@gmail.com>
2024-03-31 00:02:22 +0530

    String in Java added

---
+++ Strings/StringsInJava.java
@@ -0,0 +1,57 @@
+import java.util.*;
+public class StringsInJava {
+
+    //function to print the string using charAt() Method     
+    public static void printLetters(String str){
+        for (int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+
+
+    public static void main(String[] args) {
+        char arr[] = {'a', 'b', 'c', 'd'};  //character arrays
+
+        String str = "abcd";
+        String str2 = new String("xyz@#3588$");
+
+        // Strings are IMMUTABLE
+
+/*----------------------------------------------------------------------- */ 
+        // //Input/Output
+        // Scanner sc = new Scanner(System.in);
+
+        // System.out.println("Enter the name:");
+        // String name = sc.next();   // print only first letter
+        // System.out.println(name);
+        // sc.nextLine();  // consume the newline character
+        
+        // System.out.println("Enter address:");
+        // String address = sc.nextLine();  // print the complete line
+        // System.out.println(address);
+
+/*----------------------------------------------------------------------- */ 
+        // //To find the length of String
+
+        // String Fullname = "Anushk Sanghvi";
+        // System.out.println(Fullname.length()); 
+
+/*----------------------------------------------------------------------- */        
+        //Concatenation
+        String firstname = "Hello";
+        String lastname = "World!";
+        
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname);
+
+/*----------------------------------------------------------------------- */        
+
+        //charAt() Method
+        System.out.println(firstname.charAt(4));
+        System.out.println(fullname.charAt(5));
+
+        //function to print the string using charAt() Method------> written above
+        printLetters(fullname);
+
+    }
+}
