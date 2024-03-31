802301101ed3382f614b813865143ede90a80703
Anushk <anushksanghvi@gmail.com>
2024-03-31 13:26:57 +0530

    Largest string added

---
+++ Strings/LargestString.java
@@ -0,0 +1,17 @@
+public class LargestString {
+    public static void main(String[] args) {
+
+        // String are compared Lexicograplically
+
+        String fruits[] = {"apple", "mango", "banana"};
+        String largest = fruits[0];
+
+        for (int i=0; i<fruits.length; i++){
+            if(largest.compareTo(fruits[i]) < 0){
+                largest = fruits[i];
+            }
+        }
+
+        System.out.println(largest);
+    }
+}
