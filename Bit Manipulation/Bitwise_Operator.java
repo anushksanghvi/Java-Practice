d377c1753a90c15fe50fda2a7bb5d136fe461ea2
Anushk <anushksanghvi@gmail.com>
2024-04-12 11:15:05 +0530

    Various Bitwise Operator

---
+++ Bit Manipulation/Bitwise_Operator.java
@@ -0,0 +1,22 @@
+public class Bitwise_Operator{
+    public static void main(String[] args) {
+        // System.out.println(5 & 6);  //Binary AND (&)
+        // System.out.println(5 | 6);  //Binary OR (|)
+        // System.out.println(5 ^ 6);  //Binary XOR (^)
+
+
+
+
+
+        System.out.println(~5);     // Binary 1's Complement
+        System.out.println(~41);
+        System.out.println(~-41);
+
+
+
+
+        // System.out.println(5<<2);         //Binary Left Shift (<<)
+        // System.out.println(6>>2);         //Binary Right Shift (>>)
+
+    }
+}
