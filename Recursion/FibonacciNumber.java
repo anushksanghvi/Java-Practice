8f2d4627d173696e1bbab2a9e8e744a09f950e41
Anushk <anushksanghvi@gmail.com>
2024-04-23 18:29:12 +0530

    Printing nth Fibonacci number and Fibonacci Sequence till n

---
+++ Recursion/FibonacciNumber.java
@@ -0,0 +1,24 @@
+public class FibonacciNumber {
+    public static int fib(int n){
+        //BaseCase
+        if(n == 1 || n == 0){
+            return n;
+        }
+
+        int f1 = fib(n-1);
+        int f2 = fib(n-2);
+        return f1 + f2;
+
+        // return fib(n-1) + fib(n-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fib(6));      // print nth Fibonacci No.
+
+
+        //For printing Fibonacci Sequence till n
+        int n = 10;
+        for (int i=0; i<=n; i++){
+            System.out.print(fib(i) + " ");
+        }
+    }
+}
