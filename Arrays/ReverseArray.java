959fd38a77204cebee0dd51b58781691265989f5
Anushk Sanghvi <anushksanghvi@gmail.com>
2024-08-16 16:04:50 +0530

    Reverse Array updated

--- Arrays/ReverseArray.java
+++ Arrays/ReverseArray.java
@@ -1,26 +1,43 @@
 public class ReverseArray {
-    public static void reverseNum(int numbers[]){
-        int first=0, last=numbers.length-1;
+
+    public static int[] reverseNum1(int arr[]) {        // TC = O(n)   SC = O(n) ---> using extra space
+        int reverseArr[] = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            reverseArr[i] = arr[arr.length-1-i];
+        }
+        return reverseArr;
+    }
+
+    public static int[] reverseNum2(int arr[]){      // TC = O(n)   SC = O(1) ---> no extra space
+        int first=0, last=arr.length-1;
         while(first < last){
             //SWAP
-            int temp = numbers[last];
-            numbers[last] = numbers[first];
-            numbers[first] = temp;
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
     
             first++;
             last--;
         }
+        return arr;
+    }
+
+    public static void printArr(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
-        int numbers[] = {2,4,6,8,10,12,16};
+        int arr[] = {2,4,6,8,10,12,16};
 
-        reverseNum(numbers);
+        int ans[] = reverseNum1(arr);
+        printArr(ans);
 
-        //print
-        for (int i=0; i<numbers.length; i++){
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();
+
+        int ans1[] = reverseNum2(arr);
+        printArr(ans1);
+        
     }
 }
