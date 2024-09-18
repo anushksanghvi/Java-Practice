e7f6493e045366f7efe2643682255d61cbbe2945
Anushk Sanghvi <anushksanghvi@gmail.com>
2024-09-19 01:22:37 +0530

    Queue using JCF

---
+++ Queues/Basic.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Basic{
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        System.out.println(q);
+
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+        
+        
+        Queue<Integer> q2 = new ArrayDeque<>();
+        q2.add(5);
+        q2.add(10);
+        q2.add(15);
+        System.out.println(q2);
+    }
+}
